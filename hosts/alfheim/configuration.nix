11a936627b3ca0fe1bc536b0f4c0c1d5355dc7da
mutantmell <malaguy@gmail.com>
2022-06-23 20:11:32 -0700

    adguard-home & unbound

--- hosts/alfheim/configuration.nix
+++ hosts/alfheim/configuration.nix
@@ -20,16 +20,19 @@
   ];
 
   networking.hostName = "alfheim";
-#  networking.useDHCP = false;
+  networking.useDHCP = false;
   networking.interfaces.eth0 = {
-#    useDHCP = false;
+    useDHCP = false;
     ipv4.addresses = [{
       address = "10.0.10.2";
       prefixLength = 24;
     }];
   };
   networking.defaultGateway = "10.0.10.1";
-  networking.nameservers = [ "10.0.10.1" ];
+
+  environment.systemPackages = with pkgs; [
+    bind
+  ];
 
   services.openssh = {
     enable = true;
@@ -57,7 +60,56 @@
     enable = true;
     openFirewall = true;
   };
-#  networking.firewall.allowedTCPPorts = [ 8443 ];
+
+
+  services.adguardhome = {
+    enable = true;
+    openFirewall = true;
+    settings = {
+      dns = {
+        bind_host = "0.0.0.0";
+        bind_hosts = [ "127.0.0.1" "0.0.0.0" ];
+        upstream_dns = [ "127.0.0.1:5335" ];
+        bootstrap_dns = [ "127.0.0.1:5335" ];
+        allowed_clients = [ "127.0.0.1" "10.0.10.3" "10.0.10.1" ];
+      };
+      dhcp = {
+        enabled = false;
+        gateway_ip = "10.0.10.1";
+        subnet_mask = "255.255.255.0";
+        range_start = "10.0.10.100";
+        range_end = "10.0.10.200";
+        lease_duration = 0;
+        icmp_timeout_msec = 0;
+      };
+    };
+  };
+
+  services.unbound = {
+    enable = true;
+    settings = {
+      server = {
+        interface = [ "127.0.0.1" ];
+        port = 5335;
+        access-control = [
+          "0.0.0.0/0 refuse"
+          "127.0.0.0/8 allow"
+          "::1 allow"
+        ];
+        aggressive-nsec = true;
+        local-zone = ''"local." static'';
+        local-data = [
+          ''"local. A 10.0.10.1"''
+          ''"local. AAAA fd00::1"''
+          ''"yggdrasil.local. A 10.0.10.1"''
+          ''"yggdrasil.local. AAAA fd00::1"''
+          ''"heimdall.local. A 10.0.10.3"''
+          ''"heimdall.local. AAAA fd00::2"''
+        ];
+      };
+      remote-control.control-enable = true;
+    };
+  };
 
   # gpu accelleration
   #hardware.raspberry-pi."4".fkms-3d.enable = true;
