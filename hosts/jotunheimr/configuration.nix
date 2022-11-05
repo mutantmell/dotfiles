3a369187ad899026f3a6192061b01de2cbc4919d
mutantmell <malaguy@gmail.com>
2022-11-04 18:33:42 -0700

    WIP jotunheimr, the NAS

---
+++ hosts/jotunheimr/configuration.nix
@@ -0,0 +1,70 @@
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [
+      ./hardware-configuration.nix
+      #./nas.nix
+    ];
+
+  boot.loader.grub.enable = true;
+  boot.loader.grub.version = 2;
+  boot.loader.grub.device = "/dev/sda";
+
+  networking.hostName = "jotunheimr";
+  time.timeZone = "America/Los_Angeles";
+
+  nix.settings.auto-optimise-store = true;
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "-d";
+  };
+
+  networking.useDHCP = false;
+  #networking.interfaces.something = {
+  #  useDHCP = false;
+  #  ipv4.addresses = [{
+  #    address = "10.0.30.1";
+  #    prefixLength = 24;
+  #  }];
+  #}
+
+  environment.systemPackages = with pkgs; [
+    vim
+    wget
+  ];
+
+  services.openssh = {
+    enable = true;
+    passwordAuthentication = false;
+    permitRootLogin = "prohibit-password";
+    kbdInteractiveAuthentication = false;
+  };
+
+#  users.mutableUsers = false;
+  users.extraUsers.root.openssh.authorizedKeys.keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO22svFtlML/J11VMlNmqBkHdXH+BCWj1DXJkw+K7vbi malaguy@gmail.com"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDyEvg2vPwhxg72QgVjNzbzGd3eE0/ZjdoDawHoK24fR malaguy@gmail.com"
+  ];
+
+  users.users.mjollnir = {
+    isNormalUser = true;
+  };
+
+  services.avahi = {
+    enable = true;
+    publish = {
+      enable = true;
+      addresses = true;
+      userServices = true;
+      workstation = true;
+    };
+  };
+
+  boot.zfs.extraPools = [ "data" ];
+  services.zfs.autoScrub.enable = true;
+  
+  system.stateVersion = "21.11";
+
+}
---
+++ hosts/jotunheimr/nas.nix
@@ -0,0 +1,71 @@
+{ config, pkgs, lib, ... }:
+{
+  # services.krb5 = {
+  #   enable = true;
+  #   realms."JOTUNHEIMR.LOCAL" = {
+  #     admin_server = "jotunheimr.local";
+  #     kdc = "jotunheimr.local";
+  #   };
+  #   domain_realm."jotunheimr.local" = "JOTUNHEIMR.LOCAL";
+  #   libdefaults.default_realm = "JOTUNHEIMR.LOCAL";
+  # };
+  # services.kerberos_server = {
+  #   enable = true;
+  #   realms."JOTUNHEIMR.LOCAL".acl = [
+  #     { principal = "*/admin"; access = "all"; }
+  #     { principal = "admin"; access = "all"; }
+  #   ];
+  # };
+
+  networking.firewall.allowedTCPPorts = [
+    2049 # nfs
+    #88   # kerberos
+    #749  # kerberos admin
+  ];
+
+  ntp.enable = true;
+  services.nfs.server = {
+    enable = true;
+    createMountPoints = true;
+    exports =''
+      /data/media 10.0.20.0/24(rw,sync,no_subtree_check,no_root_squash)
+      /data/data 10.0.20.0/24(rw,sync,no_subtree_check,no_root_squash)
+    '';
+  };
+
+  
+  services.samba = {
+    enable = false;
+    securityType = "user";
+    shares = {
+      data = {
+        path = "/data/share";
+        public = false;
+        writable = true;
+      };
+      drive = {
+        path = "/some/path";
+        public = false;
+        writable = true;
+      };
+      media = {
+        path = "/some/path";
+        public = false;
+        writable = true;
+      };
+      share = {
+        path = "/some/path";
+        public = true;
+        writable = true;
+      };
+    };
+    extraConfig = ''
+      # login to guest if login fails
+      map to guest = Bad User
+      # fix error with no printers
+      load printers = no
+      printcap name = /dev/null
+      printing = bsd
+    '';
+  };
+}
