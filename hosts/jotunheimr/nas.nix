07b03912e138f35f1fac9d2031f8f11ac1bb98f0
mutantmell <malaguy@gmail.com>
2023-01-31 16:09:12 -0800

    add extra exports that are bind-mounts

--- hosts/jotunheimr/nas.nix
+++ hosts/jotunheimr/nas.nix
@@ -23,12 +23,34 @@
     #749   # kerberos admin
   ];
 
+  fileSystems."/export/ro/media" = {
+    device = "/data/media";
+    options = [ "bind" ];    
+  };
+  fileSystems."/export/rw/media" = {
+    device = "/data/media";
+    options = [ "bind" ];    
+  };
+  fileSystems."/export/ro/data" = {
+    device = "/data/data";
+    options = [ "bind" ];    
+  };
+  fileSystems."/export/rw/data" = {
+    device = "/data/data";
+    options = [ "bind" ];    
+  };
+
+  
   services.nfs.server = {
     enable = true;
 #    createMountPoints = true;
     exports =''
       /data/media 10.0.10.0/24(rw,sync,no_subtree_check,no_root_squash) 10.0.20.0/24(rw,sync,no_subtree_check,no_root_squash)
+      /export/ro/media 10.0.10.0/24(ro) 10.0.20.0/24(ro)
+      /export/rw/media 10.0.10.0/24(rw,sync,no_subtree_check,no_root_squash) 10.0.20.0/24(rw,sync,no_subtree_check,no_root_squash)
       /data/data 10.0.10.0/24(rw,sync,no_subtree_check,no_root_squash) 10.0.20.0/24(rw,sync,no_subtree_check,no_root_squash)
+      /export/ro/data 10.0.10.0/24(ro) 10.0.20.0/24(ro)
+      /export/rw/data 10.0.10.0/24(rw,sync,no_subtree_check,no_root_squash) 10.0.20.0/24(rw,sync,no_subtree_check,no_root_squash)
     '';
   };
 
