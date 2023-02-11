c0c0d50f20af74c420dd927dddf858ec949f32a2
mutantmell <malaguy@gmail.com>
2023-02-10 22:12:18 -0800

    add script that will schedule synapse compressor runs twice a week

--- cloud/matrix/matrix/synapse.nix
+++ cloud/matrix/matrix/synapse.nix
@@ -32,4 +32,30 @@
       LC_COLLATE = "C"
       LC_CTYPE = "C";
   '';
+
+  systemd.services."synapse-compress" = {
+    serviceConfig = {
+      Type = "oneshot";
+      EnvironmentFile = config.sops.secrets.synapse-compress-env.path;
+    };
+    path = with pkgs; [
+      bash
+      matrix-synapse-tools.rust-synapse-compress-state
+      postgresql
+    ];
+    script = ''
+      #!/usr/bin/env bash
+
+      synapse_auto_compressor -p postgresql://postgres:$POSTGRES_PASS@localhost/matrix-synapse -c 500 -n 1000
+      psql --host=localhost --port=5432 --dbname=matrix-synapse --username=postgres -c 'VACUUM FULL VERBOSE'
+    '';
+  };
+  systemd.timers."synapse-compress" = {
+    wantedBy = [ "timers.target" ];
+    partOf = [ "synapse-compress.service" ];
+    timerConfig = {
+      OnCalendar = "Sun,Wed *-*-* 02:00:00";
+      Unit = "synapse-compress.service";
+    };
+  };
 }
--- cloud/matrix/sops.nix
+++ cloud/matrix/sops.nix
@@ -2,6 +2,12 @@
   sops = {
     defaultSopsFile = ./secrets/secrets.yaml;
     age.sshKeyPaths = [ "/root/.ssh/id_ed25519" ];
-    secrets.wireguard-key = {};
+    secrets = {
+      wireguard-key = {};
+      synapse-compress-env = {};
+      pgpass = {
+        path = "/root/.pgpass";
+      };
+    };
   };
 }
