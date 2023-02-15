2341e3d2917e802fc7f3186c67807b22850e2d98
mutantmell <malaguy@gmail.com>
2023-02-15 08:31:57 -0800

    wireguard tests

--- hosts/muspelheim/guests/surtr/configuration.nix
+++ hosts/muspelheim/guests/surtr/configuration.nix
@@ -21,19 +21,31 @@
 
   networking.hostName = "surtr";
   networking.wireguard.interfaces = {
-    "wg0" = {
-      ips = [ "10.100.0.1/32" ];
-      listenPort = 51895;
-      privateKeyFile = config.sops.secrets."wireguard_private_key".path;
-      peers = [
-        {
-          publicKey = "QdA39mQUqQjSvOTy4c+Zrtll1OEb/4vroewi2Zz6+Qs=";
-          allowedIPs = [ "10.100.0.2/32" ];
-          endpointFile = config.sops.secrets."wireguard_peer_address".path;
-          dynamicEndpointRefreshSeconds = 15;
-        }
-      ];
-    };
+    # "wg0" = {
+    #   ips = [ "10.100.0.1/24" ];
+    #   privateKeyFile = config.sops.secrets."wireguard_private_key".path;
+    #   peers = [
+    #     {
+    #       publicKey = "QdA39mQUqQjSvOTy4c+Zrtll1OEb/4vroewi2Zz6+Qs=";
+    #       allowedIPs = [ "10.100.0.0/24" ];
+    #       endpointFile = config.sops.secrets."wireguard_peer_address".path;
+    #       dynamicEndpointRefreshSeconds = 15;
+    #       persistentKeepalive = 25;
+    #     }
+    #   ];
+    # };
+    # "wg-mx" = {
+    #   ips = [ "10.100.1.2/32" ];
+    #   privateKeyFile = config.sops.secrets."wireguard_private_key".path;
+    #   peers = [
+    #     {
+    #       publicKey = "hTmV7qOLXHCQnTWljCiNHf2P22GBd0n339Fcq4tVdlw=";
+    #       allowedIPs = [ "10.100.1.0/24" ];
+    #       endpoint = "helveticastandard.com:51895";
+    #       persistentKeepalive = 25;
+    #     }
+    #   ];
+    # };
   };
 
   services.avahi = {
