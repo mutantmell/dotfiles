9682e8660f7c3e998d9cdc40c9c6c5b186ad773a
mutantmell <malaguy@gmail.com>
2023-02-19 20:51:37 -0800

    try and work around nasty systemd bug where ppp isn't always set as route

--- hosts/muspelheim/guests/surtr/proxy.nix
+++ hosts/muspelheim/guests/surtr/proxy.nix
@@ -1,6 +1,41 @@
 { config, ... }:
 {
   config = {
+    networking.firewall.allowedTCPPorts = [ 443 4180 ]; # 4180 is temporary
+    security.acme = {
+      defaults = {
+        server = "https://alfheim.local/acme/acme/directory";
+        email = "malaguy@gmail.com";
+      };
+      acceptTerms = true;
+    };
+    services.nginx = {
+      enable = true;
+      recommendedProxySettings = true;
+      recommendedTlsSettings = true;
+
+      virtualHosts."${config.networking.hostName}.local" = {
+        forceSSL = true;
+        enableACME = true;
+
+        locations."/" = {
+          proxyPass = "http://127.0.0.1:4180";
+          extraConfig = ''
+            proxy_set_header X-Forwarded-For $proxy_protocol_addr;
+            proxy_set_header X-Forwarded-Proto $scheme;
+            proxy_set_header Host $host;
+            proxy_set_header X-Real-IP $remote_addr;
+            proxy_set_header X-Scheme $scheme;
+            proxy_connect_timeout 1;
+            proxy_send_timeout 30;
+            proxy_read_timeout 30;
+            proxy_buffer_size   128k;
+            proxy_buffers   4 256k;
+            proxy_busy_buffers_size   256k;
+          '';
+        };
+      };
+    };
     services.oauth2_proxy = {
       enable = true;
       keyFile = config.sops.secrets."oauth-2-proxy-keyfile".path;
@@ -9,8 +44,10 @@
       upstream = [
         "https://bragi.local"
       ];
-      redirectURL = "http://10.100.1.1/oauth2/callback";
+      redirectURL = "http://surtr.local:4180/oauth2/callback";
+      reverseProxy = true;
       email.domains = ["*"];
+      httpAddress = ":4180";
       extraConfig = {
         "oidc-issuer-url" = "https://alfheim.local/auth/realms/external";
         "code-challenge-method" = "S256";
