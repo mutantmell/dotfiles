2edf7c1b93ef96274fe5c781fd8e2828f6a33755
mutantmell <malaguy@gmail.com>
2023-02-21 19:49:58 -0800

    try and get nginx proxy working

--- hosts/muspelheim/guests/surtr/proxy.nix
+++ hosts/muspelheim/guests/surtr/proxy.nix
@@ -1,7 +1,7 @@
 { config, ... }:
 {
   config = {
-    networking.firewall.allowedTCPPorts = [ 443 4180 ]; # 4180 is temporary
+    networking.firewall.allowedTCPPorts = [ 80 443 4180 ]; # 4180 is temporary
     security.acme = {
       defaults = {
         server = "https://alfheim.local/acme/acme/directory";
@@ -24,12 +24,64 @@
         '';
 
         locations."/" = {
+          extraConfig = ''
+            auth_request /oauth2/auth;
+            error_page 401 = /oauth2/sign_in;
+
+            # pass information via X-User and X-Email headers to backend,
+            # requires running with --set-xauthrequest flag
+            auth_request_set $user   $upstream_http_x_auth_request_user;
+            auth_request_set $email  $upstream_http_x_auth_request_email;
+            proxy_set_header X-User  $user;
+            proxy_set_header X-Email $email;
+
+            # if you enabled --pass-access-token, this will pass the token to the backend
+            auth_request_set $token  $upstream_http_x_auth_request_access_token;
+            proxy_set_header X-Access-Token $token;
+
+            # if you enabled --cookie-refresh, this is needed for it to work with auth_request
+            auth_request_set $auth_cookie $upstream_http_set_cookie;
+            add_header Set-Cookie $auth_cookie;
+
+            # When using the --set-authorization-header flag, some provider's cookies can exceed the 4kb
+            # limit and so the OAuth2 Proxy splits these into multiple parts.
+            # Nginx normally only copies the first `Set-Cookie` header from the auth_request to the response,
+            # so if your cookies are larger than 4kb, you will need to extract additional cookies manually.
+            auth_request_set $auth_cookie_name_upstream_1 $upstream_cookie_auth_cookie_name_1;
+
+            # Extract the Cookie attributes from the first Set-Cookie header and append them
+            # to the second part ($upstream_cookie_* variables only contain the raw cookie content)
+            if ($auth_cookie ~* "(; .*)") {
+                set $auth_cookie_name_0 $auth_cookie;
+                set $auth_cookie_name_1 "auth_cookie_name_1=$auth_cookie_name_upstream_1$1";
+            }
+
+            # Send both Set-Cookie headers now if there was a second part
+            if ($auth_cookie_name_upstream_1) {
+                add_header Set-Cookie $auth_cookie_name_0;
+                add_header Set-Cookie $auth_cookie_name_1;
+            }
+            proxy_pass https://bragi;
+          '';
+        };
+
+        locations."/oauth2/" = {
           proxyPass = "http://127.0.0.1:4180";
           extraConfig = ''
             proxy_set_header Host $host;
             proxy_set_header X-Real-IP $remote_addr;
             proxy_set_header X-Scheme $scheme;
             proxy_set_header X-Auth-Request-Redirect $request_uri;
+          '';
+        };
+        locations."/oauth2/auth" = {
+          proxyPass = "http://127.0.0.1:4180";
+          extraConfig = ''
+            proxy_set_header Host $host;
+            proxy_set_header X-Real-IP $remote_addr;
+            proxy_set_header X-Scheme $scheme;
+            proxy_set_header Content-Length   "";
+            proxy_pass_request_body           off;
           '';
         };
       };
@@ -43,11 +95,9 @@
         "https://bragi.local"
       ];
       redirectURL = "http://surtr.local:4180/oauth2/callback";
-      reverseProxy = true;
       email.domains = ["*"];
       httpAddress = ":4180";
-      scope = "openid profile email";
-      cookie.domain = ".surtr.local";  # todo change
+      #cookie.domain = ".surtr.local";  # todo change
       cookie.refresh = "1m";
       cookie.expire = "30m";
       cookie.secure = false;
@@ -58,13 +108,7 @@
       extraConfig = {
         "provider-display-name" = "Keycloak";
         "oidc-issuer-url" = "https://alfheim.local/auth/realms/external";
-        #"code-challenge-method" = "S256";
-        "pass-authorization-header" = true;
-        "pass-user-headers" = true;
         "set-authorization-header" = true;
-
-        "cookie-csrf-per-request" = true;
-        "cookie-csrf-expire" = "5m";
       };
     };
   };
