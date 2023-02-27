9a457348fefb4b8f214c9a453ffcf29843265689
mutantmell <malaguy@gmail.com>
2023-02-27 12:52:29 -0800

    fix misc proxy errors

--- hosts/muspelheim/guests/surtr/proxy.nix
+++ hosts/muspelheim/guests/surtr/proxy.nix
@@ -20,17 +20,34 @@
       recommendedProxySettings = true;
       recommendedTlsSettings = true;
 
-      virtualHosts."${config.networking.hostName}.local" = {
+      virtualHosts."${config.networking.hostName}.local" = let
+        jellyfinConf = ''
+          add_header X-Frame-Options "SAMEORIGIN";
+          add_header X-XSS-Protection "1; mode=block";
+          add_header X-Content-Type-Options "nosniff";
+
+          add_header Strict-Transport-Security "max-age=31536000" always;
+
+          # Content Security Policy
+          # See: https://developer.mozilla.org/en-US/docs/Web/HTTP/CSP
+          # Enforces https content and restricts JS/CSS to origin
+          # External Javascript (such as cast_sender.js for Chromecast or YouTube embed JS for external trailers) must be whitelisted.
+          add_header Content-Security-Policy "default-src https: data: blob:; style-src 'self' 'unsafe-inline'; script-src 'self' 'unsafe-inline' https://www.gstatic.com/cv/js/sender/v1/cast_sender.js https://www.youtube.com/iframe_api https://s.ytimg.com; worker-src 'self' blob:; connect-src 'self'; object-src 'none'; frame-ancestors 'self'";
+      '';
+      in {
         forceSSL = true;
         enableACME = true;
+
         extraConfig = ''
+          proxy_read_timeout 604800;
+          proxy_send_timeout 604800;
           proxy_buffer_size   128k;
           proxy_buffers   4 256k;
           proxy_busy_buffers_size   256k;
         '';
-
         locations."/" = {
           proxyPass = "https://bragi.local";
+          extraConfig = jellyfinConf;
         };
       };
     };
@@ -48,7 +65,7 @@
       upstream = [
         "https://bragi.local"
       ];
-      redirectURL = "http://surtr.local:4180/oauth2/callback";
+      redirectURL = "http://surtr.local/oauth2/callback";
       email.domains = ["*"];
       httpAddress = ":4180";
       #cookie.domain = ".surtr.local";  # todo change
