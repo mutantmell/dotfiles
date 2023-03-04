0ced0f05601592359c056f31b98ae0b1b7e04d01
mutantmell <malaguy@gmail.com>
2023-03-03 17:33:30 -0800

    initial import of ymir

---
+++ hosts/muspelheim/guests/ymir/configuration.nix
@@ -0,0 +1,39 @@
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [
+      ./hardware-configuration.nix
+    ];
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  environment.systemPackages = with pkgs; [
+    git
+  ];
+
+  networking.hostName = "ymir";
+  time.timeZone = "UTC";
+
+  services.avahi = {
+    enable = true;
+    publish = {
+      enable = true;
+      addresses = true;
+    };
+  };
+
+  users.users.root.openssh.authorizedKeys.keys =
+    [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO22svFtlML/J11VMlNmqBkHdXH+BCWj1DXJkw+K7vbi malaguy@gmail.com"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDyEvg2vPwhxg72QgVjNzbzGd3eE0/ZjdoDawHoK24fR malaguy@gmail.com"
+    ];
+#  security.pki.certificates = [ (builtins.readFile ../../../../common/data/root_ca.crt) ];
+
+  services.openssh.enable = true;
+
+  system.stateVersion = "22.11";
+
+}
+
