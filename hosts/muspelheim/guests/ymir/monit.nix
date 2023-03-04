7dc8e9258de954b7d8407acfea4499154a26126e
mutantmell <malaguy@gmail.com>
2023-03-03 20:01:52 -0800

    add scrapers to prometheus

--- hosts/muspelheim/guests/ymir/monit.nix
+++ hosts/muspelheim/guests/ymir/monit.nix
@@ -2,7 +2,7 @@
 {
   config = {
     networking.firewall.allowedTCPPorts = [ 80 443 9001 ];
-    
+
     services.grafana = {
       enable = true;
       settings = {
@@ -25,6 +25,24 @@
           job_name = "${config.networking.hostName}_node";
           static_configs = [{
             targets = [ "127.0.0.1:${toString config.services.prometheus.exporters.node.port}" ];
+          }];
+        }
+        {
+          job_name = "jotunheimr_node";
+          static_configs = [{
+            targets = [ "jotunheimr.local:9001" ];
+          }];
+        }
+        {
+          job_name = "jotunheimr_zfs";
+          static_configs = [{
+            targets = [ "jotunheimr.local:9002" ];
+          }];
+        }
+        {
+          job_name = "jotunheimr_smartctl";
+          static_configs = [{
+            targets = [ "jotunheimr.local:9003" ];
           }];
         }
       ];
