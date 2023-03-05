955f7e72bce295ade777f8d0741116ecb7c0a9cc
mutantmell <malaguy@gmail.com>
2023-03-04 20:14:16 -0800

    add swapfile to laptop

--- hosts/svartalfheim/hardware-configuration.nix
+++ hosts/svartalfheim/hardware-configuration.nix
@@ -18,7 +18,11 @@
       fsType = "ext4";
     };
 
-  swapDevices = [ ];
+  swapDevices = [{
+    device = "/swapfile";
+    size = 8*1024;
+  }];
+
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
