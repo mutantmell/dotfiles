c96d6253135c125fc8939728e9e4d61425da65b9
mutantmell <malaguy@gmail.com>
2023-03-20 22:42:17 -0700

    update flake.lock and remove wg tunnel until networkd stops routing all traffic through it

--- hosts/jotunheimr/configuration.nix
+++ hosts/jotunheimr/configuration.nix
@@ -54,7 +54,11 @@
     };
   };
 
-  #  users.mutableUsers = false;
+  fileSystems."/mnt/backup" = {
+    device = "server:/data/backup/mutantmell";
+    fsType = "nfs";
+  };
+
   users.extraUsers.root.openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO22svFtlML/J11VMlNmqBkHdXH+BCWj1DXJkw+K7vbi malaguy@gmail.com"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDyEvg2vPwhxg72QgVjNzbzGd3eE0/ZjdoDawHoK24fR malaguy@gmail.com"
