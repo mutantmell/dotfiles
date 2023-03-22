140fb9cd45d8a9577e5120a0b0f05b907287c730
mutantmell <malaguy@gmail.com>
2023-03-22 08:51:54 -0700

    add new mx peer

--- cloud/matrix/wireguard.nix
+++ cloud/matrix/wireguard.nix
@@ -9,7 +9,7 @@
 
   networking.wireguard.interfaces = {
     wg0 = {
-      ips = [ "10.100.1.1/24" ];
+      ips = [ "10.100.1.1/24" "10.100.20.10/24" ];
       listenPort = 58156;
       privateKeyFile = config.sops.secrets.wireguard-key.path;
 
@@ -17,6 +17,10 @@
         {
           publicKey = "6Kb9OxV5mmCDt8GNTYTQi745sXI/ON7R9ZKnhuXPKiA=";
           allowedIPs = [ "10.100.1.2/32" "10.0.0.0/16" ];
+        }
+        {
+          publicKey = "8VUwFlyw+Rj6mXDbzYV51lgqWrWskFEcwNR/6M85fg0=";
+          allowedIPs = [ "10.100.20.0/24" ];
         }
       ];
     };
