f21e3b4060ebc39294e573f792ef57adb5d47d12
mutantmell <malaguy@gmail.com>
2023-04-06 08:31:34 -0700

    remove unneeded extra hosts from yggdrasil
    looks like dnsmasq is now communicating properly with avahi?

--- hosts/yggdrasil/configuration.nix
+++ hosts/yggdrasil/configuration.nix
@@ -186,8 +186,6 @@
   networking.extraHosts = ''
     10.0.10.2 alfheim
     10.0.10.2 alfheim.local
-    10.0.100.50 bragi.local
-    10.0.100.51 njord.local
   '';
 
   system.stateVersion = "21.11";
