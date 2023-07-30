b03974532473ef34991e68f7ac475118b8665403
mutantmell <malaguy@gmail.com>
2023-07-29 22:01:25 -0700

    add nidavellir

---
+++ hosts/nidavellir/configuration.nix
@@ -0,0 +1,77 @@
+{ config, pkgs, lib, nixos-hardware, sops-nix, ... }:
+
+{
+  imports = [
+    nixos-hardware.nixosModules.raspberry-pi-4
+    sops-nix.nixosModules.sops
+    ./sops.nix
+  ];
+
+  boot = {
+    kernelPackages = pkgs.linuxKernel.packages.linux_rpi4;
+    initrd.availableKernelModules = [ "xhci_pci" "usbhid" "usb_storage" ];
+    loader = {
+      grub.enable = false;
+      generic-extlinux-compatible.enable = true;
+    };
+  };
+
+  nix.settings.auto-optimise-store = true;
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "-d";
+  };
+  services.journald.extraConfig = ''
+    SystemMaxUse=100M
+    MaxFileSec=7day
+    Storage=volatile
+  '';
+
+  fileSystems = {
+    "/" = {
+      device = "/dev/disk/by-label/NIXOS_SD";
+      fsType = "ext4";
+      options = [ "noatime" ];
+    };
+  };
+
+  networking = {
+    hostName = "nidavellir";
+    wireless = {
+      enable = true;
+      environmentFile = config.sops.secrets."wpa.env".path;
+      networks."@wpa_key@" = {
+        psk = "@wpa_psk@";
+        authProtocols = [ "WPA-PSK-SHA256" ];
+        extraConfig = ''
+          ieee80211w=2
+        '';
+      };
+      interfaces = [ "wlan0" ];
+    };
+    # interfaces.wlan0 = {
+    #   useDHCP = false;
+    #   ipv4.addresses = [{
+    #     address = "10.1.20.50";
+    #     prefixLength = 24;
+    #   }];
+    # };
+  };
+
+  environment.systemPackages = with pkgs; [ vim ];
+
+  services.openssh.enable = true;
+
+  users = {
+    users.root = {
+      openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO22svFtlML/J11VMlNmqBkHdXH+BCWj1DXJkw+K7vbi malaguy@gmail.com"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDyEvg2vPwhxg72QgVjNzbzGd3eE0/ZjdoDawHoK24fR malaguy@gmail.com"
+      ];
+    };
+  };
+
+  hardware.enableRedistributableFirmware = true;
+  system.stateVersion = "23.11";
+}
---
+++ hosts/nidavellir/sops.nix
@@ -0,0 +1,9 @@
+{ config, ... }: {
+  config.sops = {
+    defaultSopsFile = ./secrets/secrets.yaml;
+    age.sshKeyPaths = [ "/root/.ssh/id_ed25519" ];
+    secrets = {
+      "wpa.env" = {};
+    };
+  };
+}
