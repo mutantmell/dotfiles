f322c1923aae3497c34cdcb8eb800ba65638e047
mutantmell <malaguy@gmail.com>
2023-09-27 23:44:20 -0700

    add python program to parse a uci file to a config file

---
+++ apps/openwrt/parse-uci.py
@@ -0,0 +1,54 @@
+import argparse
+import json
+import os
+
+def parse_uci(uci_file):
+  st = {}
+
+  with open(uci_file) as lines:
+    for line in filter(lambda x: x != "", lines):
+      [path, value] = line.strip().split("=", maxsplit=1)
+      entry = list(filter(lambda x: x not in ["", " "], value.split("'")))
+      if len(entry) == 1:
+        entry = entry[0]
+
+      split = path.split(".")
+      if len(split) == 2:
+        [module, name] = split
+        if module not in st:
+          st[module] = {}
+        if name not in st[module]:
+          st[module][name] = {"options": {}}
+        st[module][name]["type"] = entry
+      elif len(split) == 3:
+        [module, name, opt] = split
+        if module not in st:
+          st[module] = {}
+        if name not in st[module]:
+          st[module][name] = {"options": {}}
+        st[module][name]["options"][opt] = entry
+      else:
+        raise Exception(f"invalid input: {line}")
+
+  # Assume source is auto-generated, with the anonymous entries in relative order
+  return {
+    module: [ {
+      **v2,
+      **({"name": k} if not k.startswith("@") else {})
+    } for (k,v2) in v.items() ]
+    for (module,v)
+    in st.items()
+  }
+
+
+if __name__ == "__main__":
+  parser = argparse.ArgumentParser(description="Parse UCI to json")
+  parser.add_argument("--file", dest="file", metavar="FILE", type=str, help="path to UCI file to parse")
+  parser.add_argument("--output", dest="output", metavar="FILE", type=str, help="output path for the json file", default=None)
+  args = parser.parse_args()
+  parsed = parse_uci(args.file)
+  if args.output is None:
+    print(parsed)
+  else:
+    with open(args.output, 'w', encoding="utf-8") as outfile:
+      outfile.write(json.dumps(parsed, indent=2))
