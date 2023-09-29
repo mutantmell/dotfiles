5484a4025cd75baaa08cc7293898b34686fe6447
mutantmell <malaguy@gmail.com>
2023-09-29 11:09:05 -0700

    use flake-utils

--- flake.nix
+++ flake.nix
@@ -13,7 +13,7 @@
     };
     flake-utils.url = "github:numtide/flake-utils";
   };
-  outputs = { self, nixpkgs, nixpkgs-stable, nixos-hardware, home-manager, sops-nix, flake-utils }:
+  outputs = { self, nixpkgs, nixpkgs-stable, nixos-hardware, home-manager, sops-nix, flake-utils, }:
     (flake-utils.lib.eachDefaultSystem (system: let
       pkgs = nixpkgs.legacyPackages.${system};
     in {
