f0d69bc52c22f4322935ba59cffb07164871f2f3
mutantmell <malaguy@gmail.com>
2023-10-27 23:12:54 -0700

    allow passing extraModules via mk-home-config

--- home/default.nix
+++ home/default.nix
@@ -1,26 +1,26 @@
 { config, pkgs, lib, home-conf, ... }:
 
 let
-  is-graphical = home-conf.is-graphical or false;
-  langs = home-conf.langs or [];
   optional-nonnull = x: if x != null then [x] else [];
 in {
   programs.home-manager.enable = true;
 
-  home.username = home-conf.user;
-  home.homeDirectory = home-conf.home or (
-    if pkgs.stdenv.isDarwin then "/Users/${home-conf.user}" else "/home/${home-conf.user}"
-  );
-  home.stateVersion = "23.05";
+  home = {
+    username = home-conf.user;
+    homeDirectory = home-conf.home or (
+      if pkgs.stdenv.isDarwin then "/Users/${home-conf.user}" else "/home/${home-conf.user}"
+    );
+    stateVersion = "23.05";
+  };
 
   imports = [
     ./common.nix
   ] ++ (builtins.map (lang: {
-     "agda" = ./lang/agda.nix;
-  }.lang) langs) ++ (optional-nonnull ({
+    "agda" = ./lang/agda.nix;
+  }.lang) (home-conf.langs or [])) ++ (optional-nonnull ({
     "mjollnir" = ./mjollnir.nix;
   }.${home-conf.user} or null)) ++ (
-    lib.optional is-graphical ./graphical.nix
-  );
-  
+    lib.optional (home-conf.is-graphical or false) ./graphical.nix
+  ) ++ (home-conf.extraModules or []);
+
 }
