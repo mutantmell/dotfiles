fc75104f733aaf2595e968082fe3e89bb3a80eac
mutantmell <malaguy@gmail.com>
2023-11-28 11:42:22 -0800

    remove old definitions of musp. guests

--- hosts/muspelheim/guests/surtr/sops.nix
+++ hosts/muspelheim/guests/surtr/sops.nix
@@ -1,7 +1,6 @@
 {
   sops = {
     defaultSopsFile = ./secrets/secrets.yaml;
-    age.sshKeyPaths = [ "/root/.ssh/id_ed25519" ];
     secrets = {
       "wireguard_private_key" = {};
       "wg_ba_peer_1_address" = {};
