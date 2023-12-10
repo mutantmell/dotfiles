bb9853ddbd86c565b53fdb5ff17dbe081e6e53f2
mutantmell <malaguy@gmail.com>
2023-12-09 21:27:11 -0800

    move attic to data guest

--- hosts/jotunheimr/guests/hrungnir/attic.nix
+++ hosts/jotunheimr/guests/hrungnir/attic.nix
@@ -12,8 +12,16 @@
         max-size = 256 * 1024;
       };
       garbage-collection = {
-        retention-period = "31d";
+        retention-period = "3m";
       };
     };
   };
+
+  environment.persistence."/persist" = {
+    directories = [
+      { directory = "/var/lib/atticd"; user = "atticd"; group = "atticd"; }
+    ];
+  };
+
+  # TODO: nginx site "attic.hrungnir.local"
 }
