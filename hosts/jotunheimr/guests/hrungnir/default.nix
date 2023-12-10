8b8495c0daa7dd11d0688ff2d9e44ee50d033b43
mutantmell <malaguy@gmail.com>
2023-12-09 17:23:13 -0800

    add a git server
    This will likely be merged with attic

---
+++ hosts/jotunheimr/guests/hrungnir/default.nix
@@ -0,0 +1,45 @@
+{ pkgs, config, ... }:
+{
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  imports = [
+    ./microvm.nix
+    ./git.nix
+  ];
+
+  networking.hostName = "hrungnir";
+  common.openssh.enable = true;
+  services.openssh.hostKeys = [{
+    path = "/static/etc/ssh/ssh_host_ed25519_key";
+    type = "ed25519";
+  }];
+
+  systemd.network.enable = true;
+  systemd.network.networks."20-tap" = {
+    matchConfig.Type = "ether";
+    matchConfig.MACAddress = "5E:A5:4D:A3:A0:1A";
+    networkConfig = {
+      Address = [ "10.0.100.31/24" ];
+      Gateway = "10.0.100.1";
+      DNS = [ "10.0.100.1" ];
+      IPv6AcceptRA = true;
+      DHCP = "no";
+    };
+  };
+
+  time.timeZone = "UTC";
+  security.pki.certificates = [ (builtins.readFile pkgs.mmell.lib.data.certs.root) ];
+
+  environment.persistence."/persist" = {
+    hideMounts = true;
+    directories = [
+      "/var/log"
+      "/var/lib/nixos"
+      "/var/lib/systemd/coredump"
+    ];
+    files = [
+      "/etc/machine-id"
+    ];
+  };
+
+  system.stateVersion = "23.11";
+}
