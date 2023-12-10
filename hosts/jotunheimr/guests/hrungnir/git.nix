04d400c6acb303fd648c49578127f060c939d29c
mutantmell <malaguy@gmail.com>
2023-12-09 17:23:13 -0800

    add a git server
    This will likely be merged with attic

---
+++ hosts/jotunheimr/guests/hrungnir/git.nix
@@ -0,0 +1,19 @@
+{ config, pkgs, ... }:
+{
+  services.cgit."git.hrungnir.local" = {
+    enable = true;
+    scanPath = config.users.users.git.home; #"/var/lib/git";
+  };
+  
+  users.users.git = {
+    isSystemUser = true;
+    description = "git user";
+    home = "/var/lib/git";
+    shell = "${pkgs.git}/bin/git-shell";
+    group = "git";
+    openssh.authorizedKeys.keys = builtins.map (name:
+      pkgs.mmell.lib.data.keys.ssh.${name}
+    ) [ "deploy" "home" "jotunheimr" "muspelheim" "vanaheim" ];
+  };
+  users.groups.git = {};
+}
---
+++ hosts/jotunheimr/guests/hrungnir/microvm.nix
@@ -0,0 +1,33 @@
+{ pkgs, config, lib, ...}:
+{
+  microvm.shares = [{
+    source = "/nix/store";
+    mountPoint = "/nix/.ro-store";
+    tag = "ro-store";
+    proto = "virtiofs";
+  } {
+    source = "/data/guests/hrungnir/static";
+    mountPoint = "/static";
+    tag = "static";
+    proto = "virtiofs";
+  }];
+  fileSystems."/static".neededForBoot = lib.mkForce true;
+
+  microvm.volumes = [{
+    autoCreate = true;
+    mountPoint = "/persist";
+    image = "/data/guests/hrungnir/images/persist.img";
+    size = 100 * 1024;
+  }];
+  fileSystems."/persist".neededForBoot = lib.mkForce true;
+
+  microvm.mem = 521;
+  microvm.balloonMem = 512;
+
+  microvm.vcpu = 1;
+  microvm.interfaces = [{
+    type = "tap";
+    id = "vm-100-hrungnir";
+    mac = "5E:A5:4D:A3:A0:1A";
+  }];
+}
