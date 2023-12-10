ad1910dd46d4c4b17d102b2f219ce40acf3c3821
mutantmell <malaguy@gmail.com>
2023-12-10 14:06:11 -0800

    add unifi to gridr

---
+++ hosts/jotunheimr/guests/gridr/modules/unifi.nix
@@ -0,0 +1,29 @@
+{ config, pkgs, ... }:
+
+{
+  services.unifi = {
+    enable = true;
+    openFirewall = true;
+    unifiPackage = pkgs.unifi8;
+    maximumJavaHeapSize = 256;
+  };
+
+  services.nginx = {
+    enable = true;
+    recommendedProxySettings = true;
+    virtualHosts."unifi.${config.networking.hostName}.local" = {
+      locations."/" = {
+        proxyPass = "localhost:8443";
+        proxyWebsockets = true;
+      };
+    };
+  };
+
+  networking.firewall.allowedTCPPorts = [ 8443 ];
+
+  environment.persistence."/persist" = {
+    directories = [
+      { directory = "/var/lib/unifi"; user = "unifi"; group = "unifi"; }
+    ];
+  };
+}
