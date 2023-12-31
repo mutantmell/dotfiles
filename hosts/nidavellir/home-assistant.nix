28cca9291decbe36275f9388a5e2c65a491a1078
mutantmell <malaguy@gmail.com>
2023-12-30 18:45:46 -0800

    add an mqtt default config to hass

--- hosts/nidavellir/home-assistant.nix
+++ hosts/nidavellir/home-assistant.nix
@@ -26,6 +26,7 @@
       # Includes dependencies for a basic setup
       # https://www.home-assistant.io/integrations/default_config/
       default_config = {};
+      mqtt = {};
     };
     openFirewall = true;
   };
