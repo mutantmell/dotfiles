e1fa5798a8c4066d7a6b66cef1be2c6927ec7964
mutantmell <malaguy@gmail.com>
2023-12-31 13:05:50 -0800

    create new skadi on jotunheimer
    eventually to be moved back to vanaheim, post router experiments

---
+++ hosts/jotunheimr/guests/skadi/microvm.nix
@@ -0,0 +1,38 @@
+{ config, lib, ...}:
+{
+  microvm.shares = [{
+    source = "/nix/store";
+    mountPoint = "/nix/.ro-store";
+    tag = "ro-store";
+    proto = "virtiofs";
+  } {
+    source = "/persist/guests/surtr/static";
+    mountPoint = "/static";
+    tag = "static";
+    proto = "virtiofs";
+  }];
+  fileSystems."/static".neededForBoot = lib.mkForce true;
+
+  microvm.volumes = [{
+    autoCreate = true;
+    mountPoint = "/";
+    image = "/persist/guests/surtr/images/root.img";
+    size = 25 * 1024;
+  } {
+    autoCreate = true;
+    image = "/persist/guests/surtr/images/store-overlay.img";
+    mountPoint = config.microvm.writableStoreOverlay;
+    size = 75 * 1024;
+  }];
+  microvm.writableStoreOverlay = "/nix/.rw-store";
+
+  microvm.mem = 1024;
+  microvm.balloonMem = 1024 * 3;
+
+  microvm.vcpu = 2;
+  microvm.interfaces = [{
+    type = "tap";
+    id = "vm-20-skadi";
+    mac = "5E:A4:B9:D2:F8:03";
+  }];
+}
