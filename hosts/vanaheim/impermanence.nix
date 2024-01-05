5165b8ef2e624d2456a48c1bab6954d5eafabc7a
mutantmell <malaguy@gmail.com>
2024-01-04 19:31:42 -0800

    create a router config for vanaheim
    allows testing the new router module

---
+++ hosts/vanaheim/impermanence.nix
@@ -0,0 +1,25 @@
+{
+  environment.persistence."/persist" = {
+    hideMounts = true;
+    directories = [
+      "/etc/nixos"
+
+      "/var/log"
+      "/var/lib/nixos"
+      "/var/lib/systemd/coredump"
+    ];
+    files = [
+      "/etc/machine-id"
+
+      "/etc/ssh/ssh_host_ed25519_key"
+      "/etc/ssh/ssh_host_ed25519_key.pub"
+      "/etc/ssh/ssh_host_rsa_key"
+      "/etc/ssh/ssh_host_rsa_key.pub"
+      "/etc/ssh/initrd_ssh_host_ed25519_key"
+      "/etc/ssh/initrd_ssh_host_ed25519_key.pub"
+
+      "/root/.ssh/known_hosts"
+    ];
+  };
+  fileSystems."/persist".neededForBoot = true;
+}
---
+++ hosts/vanaheim/router.nix
@@ -0,0 +1,103 @@
+{ config, ... }:
+{
+  router = {
+    enable = true;
+    dns.upstream = "10.0.10.2";
+    firewall.extraForwards = [
+      {
+        ip.saddr = "10.0.10.32";
+        ip.daddr = "10.100.0.3";
+        verdict = "accept";
+      }
+      {
+        iifname = [
+          "vDMZ.lan"
+        ];
+        oifname = "wg-ba";
+        verdict = "accept";
+      }
+      {
+        iifname = "wg-ba";
+        ip.daddr = "10.0.100.40";
+        verdict = "accept";
+      }
+    ];
+    firewall.extraPreRoutes = [
+      {
+        iifname = "wg-ba";
+        tcp.dport = "22";
+        verdict.dnat = "10.0.100.40";
+      }
+    ];
+    firewall.extraPostRoutes = [
+      {
+        oifname = "wg-ba";
+        masquerade = true;
+      }
+      {
+        iifname = "wg-ba";
+        ip.daddr = "10.0.100.40";
+        masquerade = true;
+      }
+    ];
+    topology = {
+      "enp88s0" = {
+        network = { type = "disabled"; required = false; };
+        vlans = {
+          "vMGMT.lan" = {
+            tag = 10;
+            network = { type = "static"; dhcp.enable = true; static-addresses = ["10.0.10.1/24"]; trust = "management"; };
+          };
+          "vHOME.lan" = {
+            tag = 20;
+            network = { type = "static"; dhcp.enable = true; static-addresses = ["10.0.20.1/24"]; trust = "trusted"; };
+          };
+          "vADU.lan" = {
+            tag = 31;
+            network = { type = "static"; dhcp.enable = true; static-addresses = ["10.0.31.1/24"]; dns = "resolved"; trust = "untrusted"; };
+          };
+          "vDMZ.lan" = {
+            tag = 100;
+            network = {
+              type = "static";
+              static-addresses = ["10.0.100.1/24"];
+              dhcp.enable = true;
+              trust = "untrusted";
+            };
+          };
+        };
+        batmanDevice = "bat0";
+        mtu = "1536";
+      };
+      bat0 = {
+        batman = {
+          gatewayMode = "off";
+          routingAlgorithm = "batman-v";
+        };
+        network = { type = "disabled"; required = false; };
+        vlans = {
+          "vMGMT.bat0" = {
+            tag = 10;
+            network = { type = "static"; dhcp.enable = true; static-addresses = ["10.1.10.1/24"]; trust = "management"; };
+          };
+          "vHOME.bat0" = {
+            tag = 20;
+            network = { type = "static"; dhcp.enable = true; static-addresses = ["10.1.20.1/24"]; trust = "trusted"; };
+          };
+          "vGUEST.bat0" = {
+            tag = 30;
+            network = { type = "static"; dhcp.enable = true; static-addresses = ["10.1.30.1/24"]; trust = "untrusted"; };
+          };
+          "vIOT.bat0" = {
+            tag = 40;
+            network = { type = "static"; dhcp.enable = true; static-addresses = ["10.1.40.1/24"]; trust = "untrusted"; };
+          };
+          "vGAME.bat0" = {
+            tag = 41;
+            network = { type = "static"; dhcp.enable = true; static-addresses = ["10.1.41.1/24"]; trust = "untrusted"; };
+          };
+        };
+      };
+    };
+  };
+  }
