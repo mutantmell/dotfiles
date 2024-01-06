065bf5874568d67e5c026566a93871a45bdaa4e7
mutantmell <malaguy@gmail.com>
2024-01-05 23:27:15 -0800

    new config for vanaheim

--- hosts/vanaheim/configuration.nix
+++ hosts/vanaheim/configuration.nix
@@ -4,7 +4,8 @@
   imports = [
     ./hardware-configuration.nix
     ./impermanence.nix
-    ./router.nix
+    #./router.nix
+    ./microvm.nix
   ];
 
   boot.loader.systemd-boot.enable = true;
@@ -16,6 +17,7 @@
   #common.zfs.remoteUnlock.hostkey = /persist/etc/ssh/initrd_ssh_host_ed25519_key;
 
   boot.extraModprobeConfig = "options kvm_intel nested=1";
+  # todo: add after creating an initrd host key
   # boot.initrd.availableKernelModules = [ "e1000e" "8021q" ];
   # boot.initrd.systemd.network = {
   #   netdevs."20-enp88s0.10" = {
@@ -65,77 +67,6 @@
     dhcpcd.enable = false;
   };
 
-  # systemd.network = {
-  #   enable = true;
-  #   netdevs."20-br20" = {
-  #     netdevConfig.Kind = "bridge";
-  #     netdevConfig.Name = "br20";
-  #   };
-  #   netdevs."20-br100" = {
-  #     netdevConfig.Kind = "bridge";
-  #     netdevConfig.Name = "br100";
-  #   };
-  #   netdevs."20-enp88s0.10" = {
-  #     netdevConfig.Kind = "vlan";
-  #     netdevConfig.Name = "enp88s0.10";
-  #     vlanConfig.Id = 10;
-  #   };
-  #   netdevs."20-enp88s0.20" = {
-  #     netdevConfig.Kind = "vlan";
-  #     netdevConfig.Name = "enp88s0.20";
-  #     vlanConfig.Id = 20;
-  #   };
-  #   netdevs."20-enp88s0.100" = {
-  #     netdevConfig.Kind = "vlan";
-  #     netdevConfig.Name = "enp88s0.100";
-  #     vlanConfig.Id = 100;
-  #   };
-  #   networks."20-enp88s0" = {
-  #     matchConfig.Name = "enp88s0";
-  #     networkConfig.DHCP = "no";
-  #     networkConfig.LinkLocalAddressing = "no";
-  #     vlan = [
-  #       "enp88s0.10"
-  #       "enp88s0.20"
-  #       "enp88s0.100"
-  #     ];
-  #   };
-  #   networks."20-enp88s0.10" = {
-  #     matchConfig.Name = "enp88s0.10";
-  #     networkConfig.DHCP = "no";
-  #     networkConfig.IPv6PrivacyExtensions = "kernel";
-  #     networkConfig.Address = [ "10.0.10.30/24" ];
-  #     networkConfig.MulticastDNS = true;
-  #     routes = [ { routeConfig.Gateway = "10.0.10.1"; }];
-  #   };
-  #   networks."20-vm20-bridge" = {
-  #     matchConfig.Name = [ "enp88s0.20" "vm-20-*" ];
-  #     networkConfig.Bridge = "br20";
-  #     networkConfig.DHCP = "no";
-  #     networkConfig.LinkLocalAddressing = "no";
-  #     networkConfig.IPv6PrivacyExtensions = "kernel";
-  #   };
-  #   networks."20-vm100-bridge" = {
-  #     matchConfig.Name = [ "enp88s0.100" "vm-100-*" ];
-  #     networkConfig.Bridge = "br100";
-  #     networkConfig.DHCP = "no";
-  #     networkConfig.LinkLocalAddressing = "no";
-  #     networkConfig.IPv6PrivacyExtensions = "kernel";
-  #   };
-  #   networks."20-br20" = {
-  #     matchConfig.Name = "br20";
-  #     networkConfig.DHCP = "no";
-  #     networkConfig.LinkLocalAddressing = "no";
-  #     networkConfig.IPv6PrivacyExtensions = "kernel";
-  #   };
-  #   networks."20-br100" = {
-  #     matchConfig.Name = "br100";
-  #     networkConfig.DHCP = "no";
-  #     networkConfig.LinkLocalAddressing = "no";
-  #     networkConfig.IPv6PrivacyExtensions = "kernel";
-  #   };
-  # };
-  # services.resolved.enable = true;
   i18n.defaultLocale = "en_US.UTF-8";
 
   # fileSystems."/mnt/data" = {
--- hosts/vanaheim/impermanence.nix
+++ hosts/vanaheim/impermanence.nix
@@ -15,8 +15,8 @@
       "/etc/ssh/ssh_host_ed25519_key.pub"
       "/etc/ssh/ssh_host_rsa_key"
       "/etc/ssh/ssh_host_rsa_key.pub"
-      "/etc/ssh/initrd_ssh_host_ed25519_key"
-      "/etc/ssh/initrd_ssh_host_ed25519_key.pub"
+      #"/etc/ssh/initrd_ssh_host_ed25519_key"
+      #"/etc/ssh/initrd_ssh_host_ed25519_key.pub"
 
       "/root/.ssh/known_hosts"
     ];
