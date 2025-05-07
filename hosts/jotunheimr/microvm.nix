edd5cdc2234b5a1a1db2716ffecf33358e08b2d0
mutantmell <malaguy@gmail.com>
2025-05-07 01:39:45 +0000

    add workaround for persistent store

--- hosts/jotunheimr/microvm.nix
+++ hosts/jotunheimr/microvm.nix
@@ -7,4 +7,24 @@
     }) (builtins.readDir ./guests);
     autostart = builtins.attrNames vms;
   };
+
+  # https://github.com/astro/microvm.nix/issues/210#issuecomment-1979680979
+  systemd.services = let
+    script-name = "rm-skadi-store";
+  in {
+    "${script-name}" = {
+      serviceConfig = {
+        Type = "oneshot";
+        ExecStart = pkgs.writeScript "rm-image" ''
+          #!${pkgs.runtimeShell}
+          rm -f /data/guests/skadi/images/store-overlay.img
+        '';
+      };
+    };
+
+    "microvm@skadi" = {
+      after = [ "${script-name}.service" ];
+      requires = [ "${script-name}.service" ];
+    };
+  };
 }
