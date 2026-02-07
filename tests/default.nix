93c5240582db0abe199855a43f00b17eaa2f8655
mutantmell <malaguy@gmail.com>
2026-02-07 17:09:40 +0000

    fix issues with prefixes

--- tests/default.nix
+++ tests/default.nix
@@ -14,6 +14,7 @@
   router6-firewall = import ./modules/router6-firewall.nix { inherit pkgs lib; };
   router6-bond-bridge = import ./modules/router6-bond-bridge.nix { inherit pkgs lib; };
   router6-device-vlans = import ./modules/router6-device-vlans.nix { inherit pkgs lib; };
+  router6-bridge-vlan-ordering = import ./modules/router6-bridge-vlan-ordering.nix { inherit pkgs lib; };
 
   # Unit tests (pure Nix evaluation)
   nftables-dsl = import ./lib/nftables.nix { inherit pkgs lib; };
