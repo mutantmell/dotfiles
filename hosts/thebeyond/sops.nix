6f289640554bf8ddff1000e7a4233aa9dc1184ef
mutantmell <malaguy@gmail.com>
2026-02-27 02:26:11 +0000

    another reinstall, with better key management

--- hosts/thebeyond/sops.nix
+++ hosts/thebeyond/sops.nix
@@ -4,10 +4,6 @@
     defaultSopsFile = ./secrets/secrets.yaml;
     age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
     secrets = {
-      "chap-secrets" = {
-        path = "/etc/ppp/chap-secrets";
-      };
-      "pppd-userfile" = {};
       "wg-vpn-privatekey" = {
         mode = "0440";
         group = config.users.users."systemd-network".group;
@@ -16,7 +12,6 @@
         mode = "0440";
         group = config.users.users."systemd-network".group;
       };
-      "dynamic-network-env.conf" = {};
       "dyndns-host-domain" = {};
       "dyndns-host-password" = {};
     };
