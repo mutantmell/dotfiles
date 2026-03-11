6d317af24a8506813b0c0ec6aab786abaafef605
mutantmell <malaguy@gmail.com>
2026-03-11 23:30:33 +0000

    NFS and secrets

--- hosts/calvard/microvm/guests/langport/sops.nix
+++ hosts/calvard/microvm/guests/langport/sops.nix
@@ -2,9 +2,6 @@
   sops = {
     defaultSopsFile = ./secrets/secrets.yaml;
     secrets = {
-      "wireguard_private_key" = {};
-      "wg_ba_peer_1_address" = {};
-      "wg_ba_peer_2_address" = {};
       "oauth-2-proxy-keyfile" = {};
     };
   };
