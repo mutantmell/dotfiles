23fac69b77a65fa6e4002156db4e363a5199d31f
mutantmell <malaguy@gmail.com>
2026-03-16 23:46:52 +0000

    make host/user ssh keys required

--- lib/common/data/default.nix
+++ lib/common/data/default.nix
@@ -8,17 +8,7 @@ in {
   pki = {
     root = pki + "/root_ca.crt";
     intermediate = pki + "/intermediate_ca.crt";
-    sshUserCA = let
-      p = pki + "/ssh_user_ca.pub";
-    in
-      if builtins.pathExists p
-      then p
-      else null;
-    sshHostCA = let
-      p = pki + "/ssh_host_ca.pub";
-    in
-      if builtins.pathExists p
-      then p
-      else null;
+    sshUserCA = pki + "/ssh_user_ca.pub";
+    sshHostCA = pki + "/ssh_host_ca.pub";
   };
 }
