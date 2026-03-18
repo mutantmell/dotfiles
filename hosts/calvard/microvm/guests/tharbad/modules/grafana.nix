a32de74ea76c495464a8ca5049b8256f292eadad
mutantmell <malaguy@gmail.com>
2026-03-18 22:07:25 +0000

    add missing grafana secret key

--- hosts/calvard/microvm/guests/tharbad/modules/grafana.nix
+++ hosts/calvard/microvm/guests/tharbad/modules/grafana.nix
@@ -13,6 +13,7 @@
       security = {
         admin_user = "admin";
         admin_password = "$__file{${config.sops.secrets."grafana-admin-password".path}}";
+        secret_key = "$__file{${config.sops.secrets."grafana-secret-key".path}}";
       };
     };
     provision = {
--- hosts/calvard/microvm/guests/tharbad/sops.nix
+++ hosts/calvard/microvm/guests/tharbad/sops.nix
@@ -4,6 +4,7 @@
     age.sshKeyPaths = ["/static/etc/ssh/ssh_host_ed25519_key"];
     secrets = {
       "grafana-admin-password" = {};
+      "grafana-secret-key" = {};
       # TODO: add to secrets.yaml when ntfy auth is configured
       # "alertmanager-ntfy-url" = {};
       # "ntfy-auth-token" = {};
