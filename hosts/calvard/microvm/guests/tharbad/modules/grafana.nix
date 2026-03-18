c2ac1d3f368106af5d70b5c9440572062d70886c
mutantmell <malaguy@gmail.com>
2026-03-18 22:02:58 +0000

    enable grafana

---
+++ hosts/calvard/microvm/guests/tharbad/modules/grafana.nix
@@ -0,0 +1,71 @@
+{config, ...}: {
+  networking.firewall.allowedTCPPorts = [80 443];
+
+  services.grafana = {
+    enable = true;
+    settings = {
+      server = {
+        http_addr = "127.0.0.1";
+        http_port = 3000;
+        domain = "tharbad.internal";
+        root_url = "https://tharbad.internal/";
+      };
+      security = {
+        admin_user = "admin";
+        admin_password = "$__file{${config.sops.secrets."grafana-admin-password".path}}";
+      };
+    };
+    provision = {
+      enable = true;
+      datasources.settings.datasources = [
+        {
+          name = "Prometheus";
+          type = "prometheus";
+          url = "http://localhost:9090";
+          isDefault = true;
+        }
+        {
+          name = "Loki";
+          type = "loki";
+          url = "http://localhost:3100";
+        }
+        {
+          name = "Alertmanager";
+          type = "alertmanager";
+          url = "http://localhost:9093";
+          jsonData.implementation = "prometheus";
+        }
+      ];
+    };
+  };
+
+  services.nginx = {
+    enable = true;
+    recommendedTlsSettings = true;
+    recommendedProxySettings = true;
+    virtualHosts."tharbad.internal" = {
+      forceSSL = true;
+      enableACME = true;
+      locations."/" = {
+        proxyPass = "http://127.0.0.1:3000";
+        proxyWebsockets = true;
+      };
+    };
+  };
+
+  security.acme = {
+    defaults = {
+      server = "https://basel.internal/acme/acme/directory";
+      email = "malaguy@gmail.com";
+    };
+    acceptTerms = true;
+  };
+
+  environment.persistence."/persist".directories = [
+    {
+      directory = "/var/lib/grafana";
+      user = "grafana";
+      inherit (config.users.users.grafana) group;
+    }
+  ];
+}
