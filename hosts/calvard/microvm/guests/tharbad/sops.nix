b131d97aa85147784ad8143aac45a1555c782048
mutantmell <malaguy@gmail.com>
2026-03-19 00:00:07 +0000

    adjust tharbad secrets

--- hosts/calvard/microvm/guests/tharbad/sops.nix
+++ hosts/calvard/microvm/guests/tharbad/sops.nix
@@ -1,10 +1,16 @@
-{
+{config, ...}: {
   sops = {
     defaultSopsFile = ./secrets/secrets.yaml;
     age.sshKeyPaths = ["/static/etc/ssh/ssh_host_ed25519_key"];
     secrets = {
-      "grafana-admin-password" = {};
-      "grafana-secret-key" = {};
+      "grafana-admin-password" = {
+        mode = "0400";
+        owner = config.users.users.grafana.name;
+      };
+      "grafana-secret-key" = {
+        mode = "0400";
+        owner = config.users.users.grafana.name;
+      };
       # TODO: add to secrets.yaml when ntfy auth is configured
       # "alertmanager-ntfy-url" = {};
       # "ntfy-auth-token" = {};
