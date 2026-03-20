4915da685e69eb02353ed343dd8adf1e6600b991
mutantmell <malaguy@gmail.com>
2026-03-20 16:51:53 +0000

    angbar updates

--- hosts/angbar/default.nix
+++ hosts/angbar/default.nix
@@ -66,7 +66,7 @@
   services.greetd = {
     enable = true;
     settings.default_session = {
-      command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd sway";
+      command = "${pkgs.tuigreet}/bin/tuigreet --time --cmd sway";
       user = "greeter";
     };
   };
