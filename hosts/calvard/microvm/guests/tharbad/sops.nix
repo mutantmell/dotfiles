789106e2276b41e4cb43c60127ff8691c479a4e9
mutantmell <malaguy@gmail.com>
2026-03-21 13:52:12 +0000

    more secrets management

--- hosts/calvard/microvm/guests/tharbad/sops.nix
+++ hosts/calvard/microvm/guests/tharbad/sops.nix
@@ -4,6 +4,7 @@ _: {
     age.sshKeyPaths = ["/static/etc/ssh/ssh_host_ed25519_key"];
     secrets = {
       "perses-oidc-client-secret" = {};
+      "perses-encryption-key" = {};
       # TODO: add to secrets.yaml when ntfy auth is configured
       # "alertmanager-ntfy-url" = {};
       # "ntfy-auth-token" = {};
