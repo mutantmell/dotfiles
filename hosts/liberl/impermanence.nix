350f9824415f720e0d00f8a3f93fade7173425e5
mutantmell <malaguy@gmail.com>
2026-04-16 02:48:39 +0000

    fix liberl

--- hosts/liberl/impermanence.nix
+++ hosts/liberl/impermanence.nix
@@ -1,11 +1,5 @@
 _: {
-  environment.persistence."/persist" = {
-    hideMounts = true;
-    directories = [
-      "/var/log"
-      "/var/lib/nixos"
-      "/var/lib/systemd/coredump"
-      "/var/lib/samba"
-    ];
-  };
+  environment.persistence."/persist".directories = [
+    "/var/lib/samba"
+  ];
 }
