f35506ed613696aef5183e35231c383d467e06ef
mutantmell <malaguy@gmail.com>
2026-04-29 00:42:21 +0000

    fix default impermanence directories being overwritten

--- modules/common/impermanence.nix
+++ modules/common/impermanence.nix
@@ -16,24 +16,13 @@ in {
 
     directories = lib.mkOption {
       type = lib.types.listOf lib.types.str;
-      default = [
-        "/etc/nixos"
-        "/var/log"
-        "/var/lib/nixos"
-        "/var/lib/systemd/coredump"
-      ];
+      default = [];
       description = "Baseline directories to persist.";
     };
 
     files = lib.mkOption {
       type = lib.types.listOf lib.types.str;
-      default = [
-        "/etc/ssh/ssh_host_ed25519_key"
-        "/etc/ssh/ssh_host_ed25519_key.pub"
-        "/etc/ssh/ssh_host_rsa_key"
-        "/etc/ssh/ssh_host_rsa_key.pub"
-        "/root/.ssh/known_hosts"
-      ];
+      default = [];
       description = "Baseline files to persist.";
     };
   };
@@ -41,7 +30,19 @@ in {
   config = lib.mkIf cfg.enable {
     environment.persistence.${cfg.persistDir} = {
       hideMounts = true;
-      inherit (cfg) directories files;
+      directories = cfg.directories ++ [
+        "/etc/nixos"
+        "/var/log"
+        "/var/lib/nixos"
+        "/var/lib/systemd/coredump"
+      ];
+      files = cfg.files ++ [
+        "/etc/ssh/ssh_host_ed25519_key"
+        "/etc/ssh/ssh_host_ed25519_key.pub"
+        "/etc/ssh/ssh_host_rsa_key"
+        "/etc/ssh/ssh_host_rsa_key.pub"
+        "/root/.ssh/known_hosts"
+      ];
     };
 
     # Copy persisted machine-id into place before systemd starts (which would
