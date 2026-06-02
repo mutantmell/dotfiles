fb88cabab6f5243310de98a85c324b654853d5b8
mutantmell <malaguy@gmail.com>
2026-06-02 19:00:52 +0000

    remove SSH key path from arcus sops

--- hosts/arcus/sops.nix
+++ hosts/arcus/sops.nix
@@ -3,7 +3,6 @@
     defaultSopsFile = ./secrets/secrets.yaml;
     # NOTE: If this fails, it's because the key hasn't been pushed yet.
     age.keyFile = "/var/lib/sops-nix/key.txt";
-    age.sshKeyPaths = ["/etc/ssh/ssh_host_ed25519_key"];
     secrets = {
       "wg-media-privatekey" = {};
     };
