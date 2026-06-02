7086fe9fc5e985cef6dc4fc3e654a9cb8019a205
mutantmell <malaguy@gmail.com>
2026-06-02 17:56:35 +0000

    update arcus and openwrt

--- hosts/arcus/sops.nix
+++ hosts/arcus/sops.nix
@@ -1,6 +1,8 @@
 {config, ...}: {
   config.sops = {
     defaultSopsFile = ./secrets/secrets.yaml;
+    # NOTE: If this fails, it's because the key hasn't been pushed yet.
+    age.keyFile = "/var/lib/sops-nix/key.txt";
     age.sshKeyPaths = ["/etc/ssh/ssh_host_ed25519_key"];
     secrets = {
       "wg-media-privatekey" = {};
