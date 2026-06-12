9bd009af79055549d68cb818bd130f214ebf5587
mutantmell <malaguy@gmail.com>
2026-06-12 14:00:15 +0000

    enable auto-allocate-uids for edith

--- hosts/calvard/incus/guests/edith/default.nix
+++ hosts/calvard/incus/guests/edith/default.nix
@@ -20,7 +20,7 @@ in {
     limits.disk = "100GB";
   };
 
-  nix.settings.experimental-features = ["nix-command" "flakes"];
+  nix.settings.experimental-features = ["nix-command" "flakes" "auto-allocate-uids"];
 
   zramSwap = {
     enable = true;
